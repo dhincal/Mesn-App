e9efaf72b7b55b3b24d6044d88b99229caf27b25
Deniz <denizhincal05@gmail.com>
2022-04-08 00:03:37 +0300

    Add ChooseMessageStyle Search Input View

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'></link>
     </Head>
-    <div className="message-screen flex flex-row">
+    <div className="message-screen flex flex-row overflow-y-hidden">
       <div className="sidebar w-24 h-screen flex flex-col justify-between py-6 items-center border-r-2">
         <h1 className="font-bold text-xl" style={{color:'#0085FF'}}>Mess</h1>
         <div className="button-group flex flex-col content-evenly h-72 justify-between items-center">
@@ -24,7 +24,16 @@ export default function Home() {
           <h1 className="font-bold text-3xl">Message</h1>
           <a className="not-active flex w-12 h-12 rounded-2xl text-3xl items-center justify-center p-4"><i class='bx bxs-message-rounded-add opacity-25'></i></a>
         </div>
+        <div className="last-messages flex flex-col h-full items-center">
+          <div className="group input-container flex flex-row items-center rounded-xl bg-gray-100 search-input-focus:ring-1">
+            <input type="text" placeholder="Search" className="bg-gray-100 text-xl rounded-xl p-3 py-5 px-8 outline-none"/>
+            <a>
+              <i className='bx bx-search opacity-25 text-4xl mr-4'></i>
+            </a>
+          </div>
+        </div>
       </div>
+
 
     </div>    
     
