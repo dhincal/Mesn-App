0ac5f7c74daf44b8c762d0d9b59eb87709f710e7
Deniz <denizhincal05@gmail.com>
2022-04-07 21:34:51 +0300

    Add UserBrief UserName / Last Message View

--- components/UserBrief.js
+++ components/UserBrief.js
@@ -1,20 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function UserBrief(imgSrc){
+function UserBrief({imgSrc, username, lastMessage}){
     imgSrc="/../public/dorpak.jpg"
     return(
         <div>
             <Head>
 
             </Head>
-            <div className="flex flex-row h-max items-center">
+            <div className="flex flex-row h-max w-full items-center p-3.5">
                 <div className="relative flex w-max h-max">
-                    <Image src={imgSrc} className="rounded-full"width={48} height={48} />
-                    <span className="absolute h-3.5 w-3.5 inset-8 rounded-full" style={{borderWidth:'2.5px', borderColor:'white', backgroundColor:'#1EE826'}}></span>
+                    <Image src={imgSrc} className="rounded-full"width={64} height={64} />
+                    <span className="absolute h-4 w-4 inset-12 rounded-full" style={{borderWidth:'2.5px', borderColor:'white', backgroundColor:'#1EE826'}}></span>
+                </div>
+                <div className="flex flex-col ml-3 w-8/12">
+                    <p className="font-bold text-xl">{username}</p>
+                    <p className="font-semibold">{lastMessage}</p>
                 </div>
             </div>
 
         </div>
     )
 }
+
+export default UserBrief;
