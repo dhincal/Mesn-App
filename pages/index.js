6820dd73561ded1c1d6502d558965d7473e6a3bc
Deniz <denizhincal05@gmail.com>
2022-04-07 15:48:47 +0300

    Make Profile Picture Smaller

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
       <div className="sidebar w-24 h-screen flex flex-col justify-between py-6 items-center border-r-2">
         <h1 className="font-bold text-xl" style={{color:'#0085FF'}}>Mess</h1>
         <div className="button-group flex flex-col content-evenly h-72 justify-between items-center">
-          <a className="active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i class='bx bxs-message-rounded opacity-100'></i></a>
-          <a className="not-active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i class='bx bxs-phone opacity-25'></i></a>
-          <a className="not-active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i class='bx bxs-cog opacity-25'></i></a>
+          <a className="active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i className='bx bxs-message-rounded opacity-100'></i></a>
+          <a className="not-active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i className='bx bxs-phone opacity-25'></i></a>
+          <a className="not-active flex w-12 h-12 rounded-2xl text-4xl items-center justify-center p-7"><i className='bx bxs-cog opacity-25'></i></a>
         </div>
-        <Image src="/../public/adem_ilter.jpg" className="rounded-full"width={64} height={64} />
+        <Image src="/../public/adem_ilter.jpg" className="rounded-full"width={48} height={48} />
       </div>
     </div>    
     
