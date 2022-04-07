7fb624fdfb98b13ea266b9342b06529ea27292cf
Deniz <denizhincal05@gmail.com>
2022-04-07 22:40:05 +0300

    UserBrief Component Unread Message / Last Message View
    Now we can see how many messages (if there is one) is unread. Second feature is we can see when was the last message has sent.

--- components/UserBrief.js
+++ components/UserBrief.js
@@ -1,7 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-function UserBrief({imgSrc, username, lastMessage}){
+function LastTimeHandler(time){
+    
+    if(parseInt(time) <= 60){ //Convert time to minutes
+        return time+"m";
+    }
+    else if(parseInt(time / 60) <= 24){ //Convert time to hours
+        return parseInt(time / 60)+"h";
+    }
+    else if(parseInt(time / 60) >= 24 && (parseInt(time/60)/24 < 7)){ //Convert time to days
+        var days = parseInt(time / 60) / 24;
+        return days + "d";
+    }
+    else if((parseInt(time/60)/24 >= 7) && (parseInt(time/60)/24)/30 < 1){ //Convert time to weeks
+       var weeks = (parseInt(time/60)/24)/7;
+       return weeks + "w";
+    }
+    else if((parseInt(time/60)/24)/30 >= 1 && parseInt(time/60/24/30/12) < 1){ //Convert time to months
+        var months = parseInt(time/60/24/30);
+        return months + "mo"
+    }
+    else{ // Convert Time to years
+        var years = parseInt(time/60/24/30/12);
+        return years + "y";
+    }
+}
+
+function UserBrief({imgSrc, username, lastMessage, unreadMessageCount, lastTime}){
     imgSrc="/../public/dorpak.jpg"
     return(
         <div>
@@ -16,6 +42,10 @@ function UserBrief({imgSrc, username, lastMessage}){
                 <div className="flex flex-col ml-3 w-8/12">
                     <p className="font-bold text-xl">{username}</p>
                     <p className="font-semibold">{lastMessage}</p>
+                </div>
+                <div className="flex flex-col justify-between h-16 right-0 ">
+                    <p className="font-semibold text-center">{LastTimeHandler(lastTime)}</p>
+                    <p className="font-semibold text-center p-1 w-7 text-l rounded-lg" style={{backgroundColor:'#0085FF', color:'#FFFFFF'}}>{unreadMessageCount}</p>
                 </div>
             </div>
 
